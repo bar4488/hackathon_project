b19e98f118c07f734d5251aa0b84359118e41c3d
harel-weinstock <harel.w.1234@gmail.com>
2022-05-12 14:29:31 +0300

    add loadNextMeetings

--- lib/main_page/main_controller.dart
+++ lib/main_page/main_controller.dart
@@ -1,5 +1,37 @@
 import 'package:flutter/material.dart';
-
+import 'package:hackathon_project/models/meeting.dart';
+import 'package:hackathon_project/communities_db.dart';
+import 'package:hackathon_project/models/community.dart';
 class MainPageController extends ChangeNotifier {
-  
+  Future<List<Meeting>> loadNextMeetings() async
+  {
+    List<Community> communities = await database.getAllCommunities();
+    List<Meeting> meetings = [];
+    for(Community community in communities)
+      {
+        List<Meeting> someMeetings = await database.getCommunityMeetings(community.id!);
+        meetings.addAll(someMeetings);
+      }
+    return meetings;
+  }
+
+  List<Meeting> nextMeetings;
+  List<Community> communities;
+  bool loaded;
+
+  MainPageController(this.database)
+      : communities = [],
+        loaded = false,
+        nextMeetings = [] {
+    loadContent();
+  }
+
+  Future loadContent() async {
+    communities = await database.getAllCommunities();
+    loaded = true;
+    notifyListeners();
+  }
+
+  CommunitiesDatabase database;
+
 }
