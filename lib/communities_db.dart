f882f07eb2f955a451fd1be2ed36cb4e20571b4b
Bar Tzadok <bar.tzadok@fitness360now.com>
2022-05-12 13:16:03 +0300

    community mock database

--- lib/communities_db.dart
+++ lib/communities_db.dart
@@ -1,12 +1,55 @@
 import 'package:graphql_flutter/graphql_flutter.dart';
-
-
-
+import 'package:hackathon_project/models/community.dart';
+import 'package:hackathon_project/models/meeting.dart';
 
 class CommunitiesDatabase {
-  createCommunity(name, id )
-  {
-
+  Future<List<Community>> getAllCommunities() {
+    return Future.delayed(
+      Duration(milliseconds: 200),
+      () {
+        return Future.value(
+          [
+            Community(meetings: [], name: "test1"),
+            Community(meetings: [], name: "test2"),
+            Community(meetings: [], name: "test3"),
+          ],
+        );
+      },
+    );
   }
 
+  Future<List<Meeting>> getCommunityMeetings(int communityId) {
+    return Future.delayed(
+      Duration(milliseconds: 200),
+      () {
+        return Future.value(
+          [
+            Meeting(
+              name: "test1",
+              start: DateTime.now().subtract(Duration(days: 1)),
+              end: DateTime.now().subtract(Duration(hours: 23)),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<Meeting> createMeeting(int communityId, Meeting meeting) {
+    return Future.delayed(
+      Duration(milliseconds: 200),
+      () {
+        return Future.value(meeting);
+      },
+    );
+  }
+
+  Future<Meeting> updateMeeting(Meeting meeting) {
+    return Future.delayed(
+      Duration(milliseconds: 200),
+      () {
+        return Future.value(meeting);
+      },
+    );
+  }
 }
