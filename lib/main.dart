1bf9abba22978fe8d332bd539f6146ecf3825165
ionikha <Khashper@mail.tau.ac.il>
2022-05-12 12:36:38 +0300

    added graphql

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  final httpLink = HttpLink(
+    "https://api.monday.com/v2",
+  );
+  await initHiveForFlutter();
+  final AuthLink authLink = AuthLink(
+    getToken: () async =>
+        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjE2MDE2NDA5NSwidWlkIjoyOTk1NzMyNCwiaWFkIjoiMjAyMi0wNS0xMlQwNzozNjozNS45NDZaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MTE4NzU5MjIsInJnbiI6InVzZTEifQ.xtCU7DCY7Jjy0Quyx9iFsloU76Hq94xvNfpYalaN-dI',
+  );
+
+  final Link link = authLink.concat(httpLink);
+
+  runApp(
+    MyApp(
+      link: link,
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key, required this.link}) : super(key: key);
+
+  final Link link;
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final httpLink = HttpLink(
-      "https://api.monday.com/v2",
-
-    );
-
-    final AuthLink authLink = AuthLink(
-      getToken: () async => 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjE2MDE2NDA5NSwidWlkIjoyOTk1NzMyNCwiaWFkIjoiMjAyMi0wNS0xMlQwNzozNjozNS45NDZaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MTE4NzU5MjIsInJnbiI6InVzZTEifQ.xtCU7DCY7Jjy0Quyx9iFsloU76Hq94xvNfpYalaN-dI',
-
-    );
-
-    final Link link = authLink.concat(httpLink);
-
     ValueNotifier<GraphQLClient> client = ValueNotifier(
       GraphQLClient(
         link: link,
