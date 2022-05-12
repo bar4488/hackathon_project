04f348a6ef78adc9cf14a3d573ea956534c809a6
ionikha <Khashper@mail.tau.ac.il>
2022-05-12 14:18:09 +0300

    currently works, first query

--- lib/communities_db.dart
+++ lib/communities_db.dart
@@ -5,15 +5,39 @@ import 'package:hackathon_project/models/meeting.dart';
 class CommunitiesDatabase {
   CommunitiesDatabase(this.link);
   Link link;
+  final int workspace_id = 3;
 
-  Future<List<Community>> getAllCommunities() {
+  Future<GraphQLClient> getClient() async {
+    /// initialize Hive and wrap the default box in a HiveStore
+    final store = await HiveStore.open(path: 'my/cache/path');
+    return GraphQLClient(
+    /// pass the store to the cache for persistence
+    cache: GraphQLCache(store: store),
+    link: link,
+    );
+  }
+
+
+  Future<List<Community>> getAllCommunities() async {
 
     String GQLgetAllCommunities = "";
 
 
-   
+    final QueryOptions options = QueryOptions(
+      document: gql(GQLgetAllCommunities),
+      variables: <String, dynamic>{
+        'workspace_id': workspace_id
+      },
+    );
+    GraphQLClient client = await getClient();
+    final QueryResult result = await client.query(options);
 
+    if (result.hasException) {
+      print(result.exception.toString());
+    }
 
+    final List<dynamic> communities =
+    result.data?["Group"] as List<dynamic>;
 
 
     return Future.delayed(
